a1fece5531abab180cd030e836d6b2ce5450edbd
KaueFulgencio <Kauemfulgencio@outlook.com>
2024-03-25 21:34:15 -0300

    first commit

---
+++ src/account/dto/create-account.dto.ts
@@ -0,0 +1,5 @@
+export class CreateAccountDto{
+    readonly _id: string;
+    readonly email: string;
+    readonly telefone: string;
+}
---
+++ src/account/interfaces/account.interface.ts
@@ -0,0 +1,11 @@
+export interface Account {
+    readonly _id: string;
+    readonly email: string;
+    readonly telefone: string;
+    nome: string;
+    senha: string;
+    ocupacao: string;
+    endereco: string;
+    tipo: string;
+    urlFotoAccount: string;
+}
